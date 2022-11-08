a2da8c08d6a21e02d6486173f9c24e27778cdd19
Shawn19702 <shawnboodhoo5@gmail.com>
2022-11-08 15:35:14 -0500

    Pizza Api

--- src/main/java/pizza/example/pizza/controller/PizzaController.java
+++ src/main/java/pizza/example/pizza/controller/PizzaController.java
@@ -2,10 +2,7 @@ package pizza.example.pizza.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pizza.example.pizza.model.Pizza;
 import pizza.example.pizza.repository.CustomerRepository;
 import pizza.example.pizza.repository.PizzaRepository;
@@ -22,12 +19,16 @@ public class PizzaController {
 
 
     @PostMapping("/pizza/{customer_id}/pizza")
-
     public Optional<Pizza> createPizza(@PathVariable(value = "customer_id") Long customer_id, @Validated @RequestBody Pizza pizza) {
         return customerRepository.findById(customer_id).map(customer -> {
             pizza.setCustomer(customer);
             return pizzaRepository.save(pizza);
         });
+
+    }
+    @GetMapping("/pizza")
+    public Iterable<Pizza> getAllpizzas() {
+        return pizzaRepository.findAll();
 
     }
 }
